1a47432931fbc0300c70e4ecd0541363c43293c9
amaliekea <amje0001@stud.kea.dk>
2025-04-07 15:15:11 +0200

    sendquestion

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,18 +1,30 @@
 console.log("jeg er i frontpage!!");
-
-const urlQuestion = "http://localhost:8081/study-helper";
+const urlQuestion = "http://localhost:8080/study-helper";
 const sendQuestionButton = document.getElementById("sendQuestion");
+const responseElement = document.getElementById("response");
 
-sendQuestionButton.addEventListener("click", ()=> {sendQuestion()});
+sendQuestionButton.addEventListener("click", () => {
+    sendAndRecieve()
+});
 
-async function sendQuestion() {
+async function sendAndRecieve() {
     const questionInput = document.getElementById("questionInput").value; //hiver værdien ud ved .value
+    const includeQuizInput = document.getElementById("includeQuizInput").value;
+    const levelInput = document.getElementById("levelInput").value;
     console.log("knap kaldt")
-    await fetch(urlQuestion, {
+    const data = {"topic": questionInput, includeQuizInput, levelInput}
+    console.log(data)
+  const response =  await fetch(urlQuestion, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(questionInput)
+        body: JSON.stringify(data)
     })
+
+    //show response
+    const text = await response.text(); //fordi backend retunerer string
+    responseElement.textContent = text;
+    responseElement.style.display = "block";
+    console.log("Svar fra backend:", text);
 }
